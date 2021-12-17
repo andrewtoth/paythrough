531dfe39201d92aa25a820fb2501a5505cc2f4f0
Andrew Toth <andrewstoth@gmail.com>
2021-12-17 15:18:28 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# paythrough
+
+This plugin adds the ability to pay a bolt11 invoice through a specific channel,
+even if a better/cheaper route exists through a different channel. A new RPC,
+`paythrough`, is defined which takes the same arguments as `pay`, except the
+second argument is `scid` which is the short-channel-id of the channel you wish
+to pay through. This leverages the `exclude` argument on `pay`, so that argument
+is removed. Since this new argument is only available on master, this plugin is
+still experimental and is incompatible with any stable releases.
+
+This can be useful for keeping channels balanced or otherwise managing liquidity
+when you will be paying for something through lightning anyways.
---
+++ paythrough.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+'''Pay through a specific channel
+'''
+from pyln.client import Plugin
+from pyln.client.lightning import RpcError
+
+plugin = Plugin()
+
+@plugin.init()
+def init(options: dict, configuration: dict, plugin: Plugin, **kwargs):
+    return {}
+
+@plugin.method('paythrough')
+def paythrough(plugin, bolt11, scid, msatoshi=None, label=None, riskfactor=None,
+                maxfeepercent=None, retry_for=None, maxdelay=None,
+                exemptfee=None):
+    """Pay a bolt11 invoice through a specific channel,
+    even if better/cheaper routes exist through other channels.
+    All parameters after scid are identical to pay except exclude. These will
+    all be forwarded to pay via this plugin.
+    """
+    peers = plugin.rpc.listpeers()['peers']
+    channels = list(map(lambda peer: peer['channels'], peers))
+    channels = [item for sublist in channels for item in sublist]
+    channels_length = len(channels)
+    channels = list(filter(lambda channel: channel['short_channel_id'] != scid,
+                    channels))
+    
+    if len(channels) != channels_length-1: # We should have one less channel
+        return { 'code': -1, 
+                'message': f'Short channel id {scid} is not valid' }
+
+    scids = list(map(lambda channel: 
+        f"{channel['short_channel_id']}/{channel['direction']}", channels))
+
+    try:
+        resp = plugin.rpc.pay(bolt11=bolt11, msatoshi=msatoshi, label=label,
+            riskfactor=riskfactor, maxfeepercent=maxfeepercent,
+            retry_for=retry_for, maxdelay=maxdelay, exemptfee=exemptfee,
+            exclude=scids)
+        return resp
+    except RpcError as e:
+        return e.error
+
+plugin.run()
